91efb577f1730d4c34fb20147718290fa563fa9e
Your Name <tangshirong477@gmail.com>
2025-07-23 21:30:19 -0400

    react-router-dom update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
